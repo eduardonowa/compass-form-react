731034ebe21448b8725e03982b4a3160aaabb016
Eduardo Nowakoski <eduardo_nowa@hotmail.com>
2022-09-28 23:06:42 -0300

    feat: adding project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import Home from "./pages/Home/Home";
+import Error404 from "./pages/Error404/Error404";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="*" element={<Error404 />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/Error404/Error404.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Error404.scss";
+
+import Error from "../../images/error404";
+
+const Error404 = () => {
+  return <div className="error-404-container">{Error}</div>;
+};
+
+export default Error404;
